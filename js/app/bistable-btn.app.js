a21950503b1c3d33f348599c80a9aff0cc5fa59f
Nikita <nikita.umnov@mas-tech.ru>
2024-11-19 10:41:45 +0400

    удалены файлы, не относящиеся к данному модулю

--- js/app/bistable-btn.app.js
+++
@@ -1,6 +0,0 @@
-let btn = SensorManager.CreateDevice('18')[0];
-btn.Start();
-
-btn.on('enable', () => { print('on'); });
-btn.on('disable', () => { print('off'); });
-btn.on('changeState', () => { print('changeState'); });
