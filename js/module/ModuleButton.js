61b068fc94d3ec39248083d8da8483f98af9fee2
Nikita <nikita.umnov@mas-tech.ru>
2024-11-19 10:38:05 +0400

    feat: актуализация под архитектуру ModuleSensor feat: оптимизирован мониторинг кнопки за счет использования state-объекта, возвращаемого setWatch

--- js/module/ModuleButton.js
+++ js/module/ModuleButton.js
@@ -1,3 +1,16 @@
+const ClassSensor = require('ModuleSensor.min.js');
+/**
+ * @typedef TypeSetWatchParam
+ * @property {number} state – текущее состояние контакта («0» или «1»).
+ * @property {number} time – это время в секундах, когда контакт поменял состояние.
+ * @property {number} lastTime – это время в секундах, когда контакт в последний раз поменял состояние. Использование edge:'rising' или edge:'falling' – это не то же самое, что последний вызов функции.
+ * @property {number} data – этот элемент добавляется, если в аргументе options была задана настройка data:pin. С его помощью можно считывать тактовые данные.
+ */
+/**
+ * @typedef TypeConfigureParam
+ * @property {number} [debounce] - "антидребезг" (см. API setWatch())
+ * @property {number} [holdTime] - время удержания, на которое настроено событие 'hold'. | в секундах 
+ */
 /**
  * @class
  * Представляет кнопку как датчик
@@ -5,58 +18,66 @@
 class ClassButton extends ClassSensor {
     constructor(opts) {
         ClassSensor.call(this, opts);
-        this._Debounce = (opts.debounce > 0) ? opts.debounce : 50;
-        this._LastState = 1;
-        this._Time0;
-
-        this.Configure(0, { holdTime: opts.holdTime || 0.3 });
+        // задание debounce и holdTime либо в согласии с конфигом либо по умолчанию
+        this.Configure(0, { holdTime: opts.holdTime || 1.2, debounce: opts.debounce || 20 });
+        this._Pins[0].mode('input');
     }
     /**
      * @method
+     * @param {TypeSetWatchParam} _e 
      * Обработчик прерывания на пине. Вызывает события "press", "release", "click", "hold"
      */
-    OnSetWatch() {
-        let timeDelta = getTime() - this._Time0;
-        let curr_state = digitalRead(this._Pins[0]);
-
-        if (curr_state !== this._LastState) this._Channels[0].emit('changeState');
-
-        if (curr_state == 1 && this._LastState == 0) {  // кнопка отпущена
-            if (timeDelta >= this._HoldTime)            // кнопка удерживалась дольше holdTime
+    OnSetWatch(_e) {
+        if (_e.state == 1) {  // кнопка отпущена
+            if (getTime()-_e.lastTime >= this._HoldTime)            // кнопка удерживалась дольше holdTime
                 this._Channels[0].emit('hold');         
-            if (timeDelta < this._HoldTime)
-                this._Channels[0].emit('click');
-            
-            this._Channels[0].emit('release');
+            else
+                this._Channels[0].emit('click');          // иначе клик обыкновенный
         } 
         else {
             this._Channels[0].emit('press');
         }
 
-        this._Time0 = getTime();
-        this._LastState = curr_state;
-        this.Ch0_Value = curr_state;
+        this._Channels[0].Value = _e.state;
     }
-
+    /**
+     * @method 
+     * @description Запускает мониторинг порта, на котором "сидит" кнопка.
+     * @returns {Boolean}
+     */
     Start() {
         if (this._SetWatch) return false;
-        this._ChStatus[0] = 1;
+        this._Channels[0].Status = 1;
 
         this._SetWatch = setWatch(this.OnSetWatch.bind(this), 
-        this._Pins[0], {
-            repeat: true,
-            edge: 'both',
-            debounce: this._Debounce
-        });
+             this._Pins[0], 
+            {
+                repeat: true,
+                edge: 'both',
+                debounce: this._Debounce
+            }
+        );
+        return true;
     }
+
     Stop() {
         clearWatch(this._SetWatch);
         this._SetWatch = null;
-        this._ChStatus[0] = 0;
+        this._Channels[0].Status = 0;
+        return true;
     }
-    Configure(_chNum, opts) {
-        this._HoldTime = (opts.holdTime > 0.05) ? opts.holdTime : this._HoldTime;
-        this._Debounce = (typeof opts.debounce === 'number') ? opts.debounce : this._Debounce;
+    /**
+     * @method
+     * @description Позволяет настроить контроль дребезжания в setWatch при мониторинге кнопки. 
+     * Изменения вступят в силу после перезапуска опроса датчика (1. Stop() 2. Start())
+     * @param {TypeConfigureParam} _opts 
+     * @returns 
+     */
+    Configure(_chNum, _opts) {
+        // Прим! holdTime игнорируется если < 0.05; holdTime в секундах
+        this._HoldTime = (_opts.holdTime > 0.05) ? _opts.holdTime : this._HoldTime;
+        this._Debounce = (typeof _opts.debounce > 0) ? _opts.debounce : this._Debounce;
+        return true;
     }
 }
 
